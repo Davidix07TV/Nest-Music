ae3f7001d376eaeef15d625dbb23b8822085a252
Davide <davide@example.com>
2026-08-14 12:51:19 +0200

    feat: rebrand Metrolist to Nest-Music with com.nestmusic package

---
+++ app/src/main/kotlin/com/nestmusic/music/utils/potoken/PoTokenResult.kt
@@ -0,0 +1,6 @@
+﻿package com.nestmusic.music.utils.potoken
+
+class PoTokenResult(
+    val playerRequestPoToken: String,
+    val streamingDataPoToken: String,
+)
---
+++ kugou/src/main/kotlin/com/nestmusic/kugou/models/DownloadLyricsResponse.kt
@@ -0,0 +1,8 @@
+﻿package com.nestmusic.kugou.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DownloadLyricsResponse(
+    val content: String,
+)
